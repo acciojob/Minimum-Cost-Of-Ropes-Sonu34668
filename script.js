7a30b25ae5fd624e7e55c956972b7a6f9e030cb1
Sonu34668 <130197661+Sonu34668@users.noreply.github.com>
2023-08-28 11:20:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
-function calculateMinCost() {
-  //your code here
-  let string = document.getElementById("rope-lengths").value
+// function calculateMinCost() {
+//   //your code here
+//   let string = document.getElementById("rope-lengths").value
 	
-	let arr = string.split(",");
-	let finalValue =0;
-	while(arr.length > 1){
-		arr.sort((a,b) =>{return(a-b)});
-		let value = parseInt (arr.shift());
-		let value1 = parseInt(arr.shift());
-		let mainValue = value+value1;
-		finalValue = finalValue + mainValue;
-		arr.push(mainValue);
+// 	let arr = string.split(",");
+// 	let finalValue =0;
+// 	while(arr.length > 1){
+// 		arr.sort((a,b) =>{return(a-b)});
+// 		let value = parseInt (arr.shift());
+// 		let value1 = parseInt(arr.shift());
+// 		let mainValue = value+value1;
+// 		finalValue = finalValue + mainValue;
+// 		arr.push(mainValue);
 		
-	}
-  let result = document.getElementById("result");
-	result.innerText = finalValue;
-	return finalValue;
+// 	}
+//   let result = document.getElementById("result");
+// 	result.innerText = finalValue;
+// 	return finalValue;
 			
+// }
+  
+  function calculateMinCost(arr, n) {
+  // Create a min-heap to store the ropes.
+  const heap = new PriorityQueue();
+  for (let i = 0; i < n; i++) {
+    heap.push(arr[i]);
+  }
+
+  // Initialize the minimum cost to 0.
+  let minCost = 0;
+
+  // While there are more than two ropes in the heap,
+  while (heap.size() > 1) {
+    // Extract the two smallest ropes from the heap.
+    const rope1 = heap.pop();
+    const rope2 = heap.pop();
+
+    // Connect the two ropes and add the cost to the minimum cost.
+    minCost += rope1 + rope2;
+
+    // Add the combined rope back to the heap.
+    heap.push(rope1 + rope2);
+  }
+
+  // Return the minimum cost.
+  return minCost;
 }
-  
-  
